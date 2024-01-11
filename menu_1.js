ae26e730153f180251ffd433d77528435c5c8c0f
dey <david123jimenez1@hotmail.com>
2024-01-10 18:58:57 -0600

    se crea menu api

--- menu_1.js
+++ menu_1.js
@@ -1,1 +1,2 @@
 console.log("este menu solo le pertenesea menu principal")
+console.log("pero yo desde este punto del menu hare cambios")           
---
+++ menu_api.js
@@ -0,0 +1,1 @@
+console.log("este menu sera especifico de menu_api")

6a83f04999f5f0833497ba4f64f6fed9fa1d450f
dey <david123jimenez1@hotmail.com>
2024-01-12 20:21:35 -0600

    se sube a github

--- menu_1.js
+++ menu_1.js
@@ -1,6 +1,1 @@
-console.log("este menu solo le pertenesea menu principal")
-<<<<<<< HEAD
-console.log(" version 1.1 no se hara la 2")
-=======
-console.log("pero yo desde este punto del menu hare cambios")           
->>>>>>> menu_api
+console.log("este es el primer comentario que subo a git hub v:01")
